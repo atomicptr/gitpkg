ae6481d235344477a476151dca070339b029933e
Christopher Kaster <me@atomicptr.de>
2023-11-20 09:46:56 +0900

    add custom toml formatter

--- gitpkg/config.py
+++ gitpkg/config.py
@@ -32,4 +32,27 @@ class Config:
         return Binder(Config).parse_toml(path)
 
     def to_toml_string(self) -> str:
-        return "\n".join(line for line in Binder(self).format_toml())
+        lines = []
+
+        for dest_name in self.packages:
+            for pkg in self.packages[dest_name]:
+                lines.append(f"[[packages.{dest_name}]]")
+                lines.append(f'name = "{pkg.name}"')
+                lines.append(f'url = "{pkg.url}"')
+                if pkg.package_root:
+                    lines.append(f'package-root = "{pkg.package_root}"')
+                if pkg.updates_disabled:
+                    lines.append("updates-disabled = true")
+                if pkg.branch:
+                    lines.append(f'branch = "{pkg.branch}"')
+                if pkg.install_method:
+                    lines.append(f'install-method = "{pkg.install_method}"')
+                lines.append("")
+
+        for dest in self.destinations:
+            lines.append("[[destinations]]")
+            lines.append(f'name = "{dest.name}"')
+            lines.append(f'path = "{dest.path}"')
+            lines.append("")
+
+        return "\n".join(lines)
